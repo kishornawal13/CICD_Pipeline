3ca68596b5aa4a83c2619316e86df477cab7f0d8
NAWAL KISHOR <kishornawal13@gmail.com>
2026-05-06 00:07:50 +0530

    Modify deployment message in index.js
    Updated response message to reflect CI/CD deployment changes.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('🚀 Deployed via GitHub Actions to AWS Elastic Beanstalk');
+  res.send('🚀 Deployed via GitHub Actions to AWS Elastic Beanstalk. Now changes done to check CI/CD deployment.');
 });
 
 const PORT = process.env.PORT || 3000;
